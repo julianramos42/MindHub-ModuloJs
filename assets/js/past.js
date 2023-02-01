9b16a290a387e3394733f622ba0cb992c4d8de40
Julian <julianete4@gmail.com>
2023-01-31 21:47:56 -0300

    se cambian a funciones genericas para poder utilizarlas en muchas ocaciones

--- assets/js/past.js
+++ assets/js/past.js
@@ -1,32 +1,90 @@
-function createPastCards() {
-    const pastCardContainer = document.getElementById("past-card-container")
-    for (let event of data.events) {
-      if (data.currentDate > event.date) {
-        let card = document.createElement("section")
-        writeCard(card,event,pastCardContainer)
-      }
+const events = data.events
+const actualDate = data.currentDate
+const pastCardContainer = document.getElementById("past-card-container")
+
+function pastFilter(events){
+  let pastEvents = []
+  for(let event of events){
+    if(actualDate > event.date){
+      pastEvents.push(event)
     }
+  }
+  return pastEvents
 }
 
-function writeCard(card,event,container){
-    card.innerHTML = `
-          <section class="card col-lg-2 col-11">
-            <div class="card-img">
-              <img src="${event.image}" class="card-img-top" alt="${event.name}">
-            </div>
-            <div class="card-body p-0 d-flex flex-column justify-content-center">
-              <h5 class="card-title text-center m-0">${event.name}</h5>
-              <p class="text-center m-0">
-                ${event.description}
-              </p>
-              <div>
-                <p class="m-0">Price: $${event.price}</p>
-                <a href="../../assets/html/details.html" class="btn p-1">Details</a>
-              </div>
-            </div>
-          </section>
-        `
-    container.appendChild(card)
+function createCards(list){
+  for(let entrie of list){
+    writeCard(entrie,pastCardContainer)
+  }
 }
 
-createPastCards()
+function writeCard(event, container) {
+let card = document.createElement("section")
+card.innerHTML = `
+  <section class="card col-lg-2 col-11">
+    <div class="card-img">
+      <img src="${event.image}" class="card-img-top" alt="${event.name}">
+    </div>
+    <div class="card-body p-0 d-flex flex-column justify-content-center">
+      <h5 class="card-title text-center m-0">${event.name}</h5>
+      <p class="text-center m-0">
+        ${event.description}
+      </p>
+      <div>
+        <p class="m-0">Price: $${event.price}</p>
+        <a href="../../assets/html/details.html" class="btn p-1">Details</a>
+      </div>
+    </div>
+  </section>
+`
+container.appendChild(card)
+}
+
+let pastEvents = pastFilter(events)
+createCards(pastEvents)
+
+
+
+
+// const actualDate = data.currentDate
+// const events = data.events
+// const pastCardContainer = document.getElementById("past-card-container")
+// const upcomingCardContainer = document.getElementById("upcoming-card-container")
+
+// function createPastCards() {
+//   for (let event of events) {
+//     dateFilter(event)
+//   }
+// }
+
+// function dateFilter(event) {
+//   if (actualDate < event.date) {
+//     //writeCard(event, upcomingCardContainer)
+//   }else{
+//     writeCard(event, pastCardContainer)
+//   }
+// }
+
+// function writeCard(event, container) {
+//   let card = document.createElement("section")
+//   card.innerHTML = `
+//     <section class="card col-lg-2 col-11">
+//       <div class="card-img">
+//         <img src="${event.image}" class="card-img-top" alt="${event.name}">
+//       </div>
+//       <div class="card-body p-0 d-flex flex-column justify-content-center">
+//         <h5 class="card-title text-center m-0">${event.name}</h5>
+//         <p class="text-center m-0">
+//           ${event.description}
+//         </p>
+//         <div>
+//           <p class="m-0">Price: $${event.price}</p>
+//           <a href="../../assets/html/details.html" class="btn p-1">Details</a>
+//         </div>
+//       </div>
+//     </section>
+//   `
+//   container.appendChild(card)
+// }
+
+// createPastCards()
--- assets/js/upcoming.js
+++ assets/js/upcoming.js
@@ -1,32 +1,67 @@
-function createUpcomingCards() {
-    const upcomingCardContainer = document.getElementById("upcoming-card-container")
-    for (let event of data.events) {
-      if (data.currentDate < event.date) {
-        let card = document.createElement("section")
-        writeCard(card,event,upcomingCardContainer)
-      }
+const events = data.events
+const actualDate = data.currentDate
+const upcomingCardContainer = document.getElementById("upcoming-card-container")
+
+function upcomingFilter(events){
+  let upcomingEvents = []
+  for(let event of events){
+    if(actualDate < event.date){
+      upcomingEvents.push(event)
     }
+  }
+  return upcomingEvents
 }
 
-function writeCard(card,event,container){
-    card.innerHTML = `
-          <section class="card col-lg-2 col-11">
-            <div class="card-img">
-              <img src="${event.image}" class="card-img-top" alt="${event.name}">
-            </div>
-            <div class="card-body p-0 d-flex flex-column justify-content-center">
-              <h5 class="card-title text-center m-0">${event.name}</h5>
-              <p class="text-center m-0">
-                ${event.description}
-              </p>
-              <div>
-                <p class="m-0">Price: $${event.price}</p>
-                <a href="../../assets/html/details.html" class="btn p-1">Details</a>
-              </div>
-            </div>
-          </section>
-        `
-    container.appendChild(card)
+function createCards(list){
+  for(let entrie of list){
+    writeCard(entrie,upcomingCardContainer)
+  }
 }
 
-createUpcomingCards()
+function writeCard(event, container) {
+let card = document.createElement("section")
+card.innerHTML = `
+  <section class="card col-lg-2 col-11">
+    <div class="card-img">
+      <img src="${event.image}" class="card-img-top" alt="${event.name}">
+    </div>
+    <div class="card-body p-0 d-flex flex-column justify-content-center">
+      <h5 class="card-title text-center m-0">${event.name}</h5>
+      <p class="text-center m-0">
+        ${event.description}
+      </p>
+      <div>
+        <p class="m-0">Price: $${event.price}</p>
+        <a href="../../assets/html/details.html" class="btn p-1">Details</a>
+      </div>
+    </div>
+  </section>
+`
+container.appendChild(card)
+}
+
+let upcomingEvents = upcomingFilter(events)
+createCards(upcomingEvents)
+
+
+
+// const actualDate = data.currentDate
+// const events = data.events
+// const pastCardContainer = document.getElementById("past-card-container")
+// const upcomingCardContainer = document.getElementById("upcoming-card-container")
+
+// function createUpcomingCards() {
+//   for (let event of events) {
+//     dateFilter(event)
+//   }
+// }
+
+// function dateFilter(event) {
+//   if (actualDate < event.date) {
+//     writeCard(event, upcomingCardContainer)
+//   }else{
+  //     //writeCard(event, pastCardContainer)
+  //   }
+  // }
+  
+// createUpcomingCards()
