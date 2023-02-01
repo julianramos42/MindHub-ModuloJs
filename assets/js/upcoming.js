0589c68b3f4c246a60d8a887f9549db5642915f7
Julian <julianete4@gmail.com>
2023-02-01 10:38:53 -0300

    se cambia la forma en la que se crean las cards

--- assets/js/upcoming.js
+++ assets/js/upcoming.js
@@ -19,49 +19,24 @@ function createCards(list){
 }
 
 function writeCard(event, container) {
-let card = document.createElement("section")
-card.innerHTML = `
-  <section class="card col-lg-2 col-11">
-    <div class="card-img">
-      <img src="${event.image}" class="card-img-top" alt="${event.name}">
-    </div>
-    <div class="card-body p-0 d-flex flex-column justify-content-center">
-      <h5 class="card-title text-center m-0">${event.name}</h5>
-      <p class="text-center m-0">
-        ${event.description}
-      </p>
-      <div>
-        <p class="m-0">Price: $${event.price}</p>
-        <a href="../../assets/html/details.html" class="btn p-1">Details</a>
+  upcomingCardContainer.innerHTML += `
+    <section class="card col-lg-2 col-11">
+      <div class="card-img">
+        <img src="${event.image}" class="card-img-top" alt="${event.name}">
       </div>
-    </div>
-  </section>
-`
-container.appendChild(card)
+      <div class="card-body p-0 d-flex flex-column justify-content-center">
+        <h5 class="card-title text-center m-0">${event.name}</h5>
+        <p class="text-center m-0">
+          ${event.description}
+        </p>
+        <div>
+          <p class="m-0">Price: $${event.price}</p>
+          <a href="../../assets/html/details.html" class="btn p-1">Details</a>
+        </div>
+      </div>
+    </section>
+  `  
 }
 
 let upcomingEvents = upcomingFilter(events)
 createCards(upcomingEvents)
-
-
-
-// const actualDate = data.currentDate
-// const events = data.events
-// const pastCardContainer = document.getElementById("past-card-container")
-// const upcomingCardContainer = document.getElementById("upcoming-card-container")
-
-// function createUpcomingCards() {
-//   for (let event of events) {
-//     dateFilter(event)
-//   }
-// }
-
-// function dateFilter(event) {
-//   if (actualDate < event.date) {
-//     writeCard(event, upcomingCardContainer)
-//   }else{
-  //     //writeCard(event, pastCardContainer)
-  //   }
-  // }
-  
-// createUpcomingCards()
